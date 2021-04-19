df635947ecff1dc512928fd1bcaa58b1a7f7929b
PC\jihyun <coco092615@gmail.com>
2021-04-19 23:39:24 +0900

    [15퍼즐]  단어 섞기 기능 추가
    10개 정도의 임의의 단어들 중에서 무작위로 문제 하나를 출제하는 기능 추가
    새로고침 할때마다 철자가 섞이도록 구현

--- 자바스크립트/4강 실습/15-puzzle.js
+++ 자바스크립트/4강 실습/15-puzzle.js
@@ -1,31 +1,44 @@
-var str = document.getElementById('word1').innerHTML
+var word = document.getElementById('word1')
 var word2 = document.getElementById('word2')
 var check = document.getElementById('check')
 
-var game = {
-    'answer' : str
-};
-game.word = str.split('');
+var wordlist = 'almond,headphone,horizon,speaker,tissue,hat,socks,clothes'.split(',')
+
+var game = {};
+game.choice = function () {
+    var idx = Math.floor(Math.random() * wordlist.length)
+    return wordlist[idx]
+}
+var answer = game.choice()
+
+word.innerHTML = answer
+game.word = answer.split('')
 game.btns = [];
 
-game.updateDisplay = function() {
-    if (this.answer === this.word.join('')) {
+game.updateDisplay = function () {
+    if (answer === this.word.join('')) {
         check.innerHTML = "일치합니다."
     } else {
         check.innerHTML = "일치하지 않습니다."
     }
 };
 
-for (var i = 0; i < str.length; i++) {
+for (var i = 0; i < answer.length; i++) {
     var btn = document.createElement("button");
-    btn.innerHTML = str[i]
+    btn.innerHTML = answer[i]
     word2.appendChild(btn)
     game.btns.push(btn)
 };
 
+game.copyBtnText = function () {
+    for (var i = 0; i < this.word.length; i++) {
+        this.btns[i].innerHTML = this.word[i];
+    }
+};
+
 var swap = function (event) {
     var temp = [];
-    while(game.word.length !=0) {
+    while (game.word.length != 0) {
         var s = game.word.pop();
         temp.push(s);
     }
@@ -35,11 +48,7 @@ var swap = function (event) {
     game.updateDisplay();
 };
 
-game.copyBtnText = function() {
-    for (var i = 0; i < this.word.length; i++) {
-        this.btns[i].innerHTML = this.word[i];
-    }
-}
+
 
 var Rshift = function (event) {
     var s = game.word.pop();
@@ -54,3 +63,17 @@ var Lshift = function (event) {
     game.copyBtnText();
     game.updateDisplay();
 };
+
+//shuffle
+
+var toggle = Math.floor(Math.random()*2) === 0
+
+if (toggle) {
+   swap(); 
+}
+
+var n = Math.floor(Math.random()*game.word.lenght)
+
+for (i= 0; i < n; i++) { 
+    Rshift();    
+}
